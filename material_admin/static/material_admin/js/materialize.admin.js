8b7350d74196ab0fba708e74b86e144d1ac4edf5
Anton <amaistrenko@s-pro.io>
2018-09-14 17:04:04 +0300

    Menu styles.

---
+++ material_admin/static/material_admin/js/materialize.admin.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.collapsible');
+    var instances = M.Collapsible.init(elems, {
+        accordion: false
+    });
+});
