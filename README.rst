15d4628730a2f7e01696d639ce2b0b7dc9940753
Anton <it2015maistrenko@gmail.com>
2018-09-16 16:17:42 +0300

    Update setup.

---
+++ README.rst
@@ -0,0 +1,26 @@
+
+=====
+Django material admin
+=====
+
+Coming soon
+
+Quick start
+-----------
+
+1. Add "django-material-admin" to your INSTALLED_APPS setting like this::
+
+    INSTALLED_APPS = [
+        'django-material-admin',
+        'django.contrib.admin',
+        ...
+    ]
+
+2. Include the material templates URLconf in your project urls.py like this::
+
+    path('admin/', include('material_admin.urls')),
+
+
+## Django Material Administration
+
+![Layout](https://github.com/MaistrenkoAnton/django-material-admin/blob/master/demo/screens/login.png)
--- setup.py
+++ setup.py
@@ -1,18 +1,24 @@
-import setuptools
+import os
+from setuptools import find_packages, setup
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
+with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
+    README = readme.read()
 
-setuptools.setup(
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+
+setup(
     name="django-material-admin",
     version="0.0.1",
+    packages=find_packages(),
     author="Anton Maistrenko",
+    include_package_data=True,
     author_email="it2015maistrenko@gmail.com",
     description="Material Design For Django Administration",
-    long_description=long_description,
+    long_description=README,
     long_description_content_type="text/markdown",
     url="https://github.com/MaistrenkoAnton/django-material-admin",
-    packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3.6",
         'Framework :: Django 2.0',
