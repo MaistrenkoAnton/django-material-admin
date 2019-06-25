15df0f5c4e0cd93699ebc8614781b96595eb2c67
Anton <amaistrenko@s-pro.io>
2019-06-25 10:53:43 +0300

    Create meterialize init file.

--- demo/settings.py
+++ demo/settings.py
@@ -124,5 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
--- demo/urls.py
+++ demo/urls.py
@@ -20,4 +20,6 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', include('material.urls')),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(
+    settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
+)
