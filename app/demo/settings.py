2acb159052cce4ad5a00113a9882ae9c9c8e0edf
Anton <it2015maistrenko@gmail.com>
2019-06-26 10:17:44 +0300

    Fix Confirmation Delete Card (#12)
    * Init
    
    * Enhance delete confiramtion styles
    
    * Dockerize project

--- app/demo/settings.py
+++ app/demo/settings.py
@@ -80,8 +80,11 @@ WSGI_APPLICATION = 'demo.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'HOST': os.environ.get('DB_HOST'),
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASS'),
     }
 }
 
