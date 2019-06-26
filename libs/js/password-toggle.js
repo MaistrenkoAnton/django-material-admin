02e81ae4dc73e359359f1683a645ad50f6836d70
Anton <it2015maistrenko@gmail.com>
2019-06-26 18:07:28 +0300

    Related objects fixing (#13)
    * Fix mockuping
    
    * Fix mockuping

--- libs/js/password-toggle.js
+++ libs/js/password-toggle.js
@@ -1,15 +1,17 @@
-var input = document.getElementById("password-input");
-var visibleOff = document.getElementById("visible-off");
-var visibleOn = document.getElementById("visible-on");
+(function () {
+    var input = document.getElementById("password-input");
+    var visibleOff = document.getElementById("visible-off");
+    var visibleOn = document.getElementById("visible-on");
 
-function passwordShow () {
-  input.type = "text";
-  visibleOn.classList.add("hide");
-  visibleOff.classList.remove("hide");
-}
+    function passwordShow () {
+      input.type = "text";
+      visibleOn.classList.add("hide");
+      visibleOff.classList.remove("hide");
+    }
 
-function passwordHide() {
-  input.type = "password";
-  visibleOff.classList.add("hide");
-  visibleOn.classList.remove("hide");
-}
+    function passwordHide () {
+      input.type = "password";
+      visibleOff.classList.add("hide");
+      visibleOn.classList.remove("hide");
+    }
+})();
