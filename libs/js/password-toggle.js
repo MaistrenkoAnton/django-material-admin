f4efcd8d0b850b7a16cf9c0f2314ebe8ad8cee90
Anton <amaistrenko@s-pro.io>
2019-06-26 14:32:37 +0300

    Add eye icon to login password

---
+++ libs/js/password-toggle.js
@@ -0,0 +1,15 @@
+var input = document.getElementById("password-input");
+var visibleOff = document.getElementById("visible-off");
+var visibleOn = document.getElementById("visible-on");
+
+function passwordShow () {
+  input.type = "text";
+  visibleOn.classList.add("hide");
+  visibleOff.classList.remove("hide");
+}
+
+function passwordHide() {
+  input.type = "password";
+  visibleOff.classList.add("hide");
+  visibleOn.classList.remove("hide");
+}
