c0088c61ee9cfd54c6df720edd5f70fb053075a3
Anton <it2015maistrenko@gmail.com>
2019-06-28 15:39:44 +0300

    Fix navbar responsive (#16)

--- app/material/static/material/js/admin.js
+++ app/material/static/material/js/admin.js
@@ -1,37 +1,39 @@
-const toasts = ['success', 'warning']
+(function($) {
+    const toasts = ['success', 'warning']
 
-$('#side-bar, #mobile-demo').mouseenter(
-    function() {
-    $('.scroll-pane').jScrollPane();
-}
-).mouseleave(
-    function() {
-        const jsp = $('.scroll-pane').data('jsp')
-        if (jsp) {
-            jsp.destroy();
-        }
+    $('#side-bar, #mobile-demo').mouseenter(
+        function() {
+        $('.scroll-pane').jScrollPane();
     }
-);
-$('.collapsible-body > .active').closest('.scrollspy').addClass('active');
+    ).mouseleave(
+        function() {
+            const jsp = $('.scroll-pane').data('jsp')
+            if (jsp) {
+                jsp.destroy();
+            }
+        }
+    );
+    $('.collapsible-body > .active').closest('.scrollspy').addClass('active');
 
-document.addEventListener('DOMContentLoaded', function() {
-    var collapsible = document.querySelectorAll('.collapsible');
-     M.Collapsible.init(collapsible, {
-        accordion: false
+    window.addEventListener('load', function() {
+        var collapsible = document.querySelectorAll('.collapsible');
+         M.Collapsible.init(collapsible, {
+            accordion: false
+        });
+
+        var sidenav = document.querySelectorAll('.sidenav');
+        M.Sidenav.init(sidenav);
+
+        var elems = document.querySelectorAll('.dropdown-trigger');
+        M.Dropdown.init(elems);
+        $('select').not('.empty-form select, .selector-available select, .selector-chosen select').formSelect();
+
+        for(const toast of toasts) {
+            const messages = $(`.messagelist > .${toast}`);
+            for(let message of messages) {
+                M.toast({html: message.innerText, classes: `rounded ${toast}-toast`});
+            }
+        }
+
     });
-
-    var sidenav = document.querySelectorAll('.sidenav');
-    M.Sidenav.init(sidenav);
-
-    var elems = document.querySelectorAll('.dropdown-trigger');
-    M.Dropdown.init(elems);
-    $('select').not('.empty-form select').formSelect();
-
-    for(const toast of toasts) {
-        const messages = $(`.messagelist > .${toast}`);
-        for(let message of messages) {
-            M.toast({html: message.innerText, classes: `rounded ${toast}-toast`});
-        }
-    }
-
-});
+})(jQuery);
