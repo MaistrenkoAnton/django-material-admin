cb7e07abc3a3a671b12436ceeeca6e1a66526e96
Anton <it2015maistrenko@gmail.com>
2019-06-29 11:01:54 +0300

    Handle import fails for auth and sits. Bump 0.0.17

--- app/material/admin.py
+++ app/material/admin.py
@@ -1,21 +1,29 @@
-from django.contrib.auth.admin import UserAdmin, GroupAdmin
-from django.contrib.auth.models import User, Group
-from django.contrib.sites.models import Site
-
 from material.decorators import register
 from material.options import MaterialModelAdmin
 
+try:
+    from django.contrib.sites.models import Site
 
-@register(User)
-class MaterialUserAdmin(MaterialModelAdmin, UserAdmin):
-    """Register User model with material styles"""
+    @register(Site)
+    class MaterialSiteAdmin(MaterialModelAdmin):
+        """Register Site model with material styles"""
+
+except RuntimeError:
+    pass
+
+try:
+    from django.contrib.auth.admin import UserAdmin, GroupAdmin
+    from django.contrib.auth.models import User, Group
 
 
-@register(Group)
-class MaterialGroupAdmin(MaterialModelAdmin, GroupAdmin):
-    """Register Group model with material styles"""
+    @register(User)
+    class MaterialUserAdmin(MaterialModelAdmin, UserAdmin):
+        """Register User model with material styles"""
 
 
-@register(Site)
-class MaterialSiteAdmin(MaterialModelAdmin):
-    """Register Site model with material styles"""
+    @register(Group)
+    class MaterialGroupAdmin(MaterialModelAdmin, GroupAdmin):
+        """Register Group model with material styles"""
+
+except RuntimeError:
+    pass
