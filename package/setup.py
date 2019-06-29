9acaf7e0baee4917eedf4419a86f09103dda9c43
Anton <it2015maistrenko@gmail.com>
2019-06-29 21:13:18 +0300

    Scrollpane Fixing (#18)
    * Refactor, fix scrollpan.
    
    * Remove welocome message
    
    * Bump 0.0.18

--- package/setup.py
+++ package/setup.py
@@ -10,7 +10,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name="django-material-admin",
-    version="0.0.17",
+    version="0.0.18",
     packages=find_packages(),
     author="Anton Maistrenko",
     include_package_data=True,
