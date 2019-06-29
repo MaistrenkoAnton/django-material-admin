bd56f01197e95248d47904b758da1df3a0a42586
Anton <it2015maistrenko@gmail.com>
2019-06-29 10:05:17 +0300

    Dump to 0.0.16

--- package/setup.py
+++ package/setup.py
@@ -10,7 +10,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name="django-material-admin",
-    version="0.0.15",
+    version="0.0.16",
     packages=find_packages(),
     author="Anton Maistrenko",
     include_package_data=True,
