2f4a64f37e36e37bcdf33ddd3fda4d3519c491e5
Anton <it2015maistrenko@gmail.com>
2019-06-30 10:08:54 +0300

    Init.

---
+++ app/material/static/material/js/nav-bar.js
@@ -0,0 +1,44 @@
+(function ($) {
+
+    function createCookie(name, value, days) {
+        var expires;
+
+        if (days) {
+            var date = new Date();
+            date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+            expires = "; expires=" + date.toGMTString();
+        } else {
+            expires = "";
+        }
+        document.cookie = encodeURIComponent(name) + "=" + encodeURIComponent(value) + expires + "; path=/";
+    }
+
+    function readCookie(name) {
+        var nameEQ = encodeURIComponent(name) + "=";
+        var ca = document.cookie.split(';');
+        for (var i = 0; i < ca.length; i++) {
+            var c = ca[i];
+            while (c.charAt(0) === ' ')
+                c = c.substring(1, c.length);
+            if (c.indexOf(nameEQ) === 0)
+                return decodeURIComponent(c.substring(nameEQ.length, c.length));
+        }
+        return null;
+    }
+
+    function eraseCookie(name) {
+        createCookie(name, "", -1);
+    }
+
+    $('.hide-side_nav').click(function () {
+        var duration = null;
+        $('#side-bar').toggle();
+        $('#container').toggleClass('side_nav_shown');
+        var state = readCookie('side_nav');
+        if (state) {
+            eraseCookie('side_nav')
+        } else {
+            createCookie('side_nav', 'hidden', duration);
+        }
+    });
+})(jQuery)
