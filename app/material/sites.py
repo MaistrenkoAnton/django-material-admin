02e50251021c68aaa867744630deedb33b11f360
Anton <it2015maistrenko@gmail.com>
2019-07-03 18:50:55 +0300

    Material Admin Model Icons

--- app/material/sites.py
+++ app/material/sites.py
@@ -1,8 +1,9 @@
 from django.contrib.admin.sites import AdminSite
+from django.urls import NoReverseMatch, reverse
 from django.utils.functional import LazyObject
-from django.utils.html import strip_tags
 from django.utils.module_loading import import_string
 from django.apps import apps
+from django.utils.text import capfirst
 
 
 class MaterialAdminSite(AdminSite):
@@ -24,12 +25,68 @@ class MaterialAdminSite(AdminSite):
         Build the app dictionary. The optional `label` parameter filters models
         of a specific app. Adding material icons, default icons.
         """
-        app_dict = super()._build_app_dict(request, label)
-        if not label:
-            for key in app_dict.keys():
-                icon = getattr(apps.get_app_config(key), 'icon_name', None) or self.default_config_mapping.get(key)
-                if icon:
-                    app_dict[key]['icon'] = strip_tags(icon)
+        app_dict = {}
+
+        if label:
+            models = {
+                m: m_a for m, m_a in self._registry.items()
+                if m._meta.app_label == label
+            }
+        else:
+            models = self._registry
+
+        for model, model_admin in models.items():
+            app_label = model._meta.app_label
+
+            has_module_perms = model_admin.has_module_permission(request)
+            if not has_module_perms:
+                continue
+
+            perms = model_admin.get_model_perms(request)
+
+            # Check whether user has any perm for this module.
+            # If so, add the module to the model_list.
+            if True not in perms.values():
+                continue
+
+            info = (app_label, model._meta.model_name)
+            model_dict = {
+                'name': capfirst(model._meta.verbose_name_plural),
+                'object_name': model._meta.object_name,
+                'perms': perms,
+                'icon': getattr(model_admin, 'icon_name', None)
+            }
+            if perms.get('change') or perms.get('view'):
+                model_dict['view_only'] = not perms.get('change')
+                try:
+                    model_dict['admin_url'] = reverse('admin:%s_%s_changelist' % info, current_app=self.name)
+                except NoReverseMatch:
+                    pass
+            if perms.get('add'):
+                try:
+                    model_dict['add_url'] = reverse('admin:%s_%s_add' % info, current_app=self.name)
+                except NoReverseMatch:
+                    pass
+
+            if app_label in app_dict:
+                app_dict[app_label]['models'].append(model_dict)
+            else:
+                app_dict[app_label] = {
+                    'name': apps.get_app_config(app_label).verbose_name,
+                    'app_label': app_label,
+                    'app_url': reverse(
+                        'admin:app_list',
+                        kwargs={'app_label': app_label},
+                        current_app=self.name,
+                    ),
+                    'has_module_perms': has_module_perms,
+                    'models': [model_dict],
+                    'icon': getattr(apps.get_app_config(app_label), 'icon_name', None)
+                    or self.default_config_mapping.get(app_label)
+                }
+
+        if label:
+            return app_dict.get(label)
         return app_dict
 
 
