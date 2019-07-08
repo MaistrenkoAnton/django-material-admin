b1ff96567d2da69a1965551ae8dd91fa2a2fc866
Anton <it2015maistrenko@gmail.com>
2019-07-08 11:13:54 +0300

    Add default material and profile user_picture template.

--- material/templatetags/material.py
+++ material/templatetags/material.py
@@ -1,6 +1,9 @@
 import re
 
+from django import template
 from django.template import Library
+from django.template.defaultfilters import stringfilter
+
 register = Library()
 
 
@@ -12,3 +15,13 @@ def admin_change_list_value(result_checkbox_html):
     """Extract value from rendered admin list action checkbox."""
     value = CL_VALUE_RE.findall(result_checkbox_html)
     return value[0] if value else None
+
+
+@register.filter
+@stringfilter
+def template_exists(value):
+    try:
+        template.loader.get_template(value)
+        return True
+    except template.TemplateDoesNotExist:
+        return False
