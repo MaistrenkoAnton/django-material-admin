88736704f2b84a8374e5649dbfe344e9bfcc3a1e
Anton <it2015maistrenko@gmail.com>
2019-07-10 13:51:00 +0300

    Enhance error styles. Add link to File download. Bump to v1.1.11

--- package/setup.py
+++ package/setup.py
@@ -10,7 +10,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name="django-material-admin",
-    version="1.1.10",
+    version="1.1.11",
     license='MIT License',
     packages=find_packages(),
     author="Anton Maistrenko",
