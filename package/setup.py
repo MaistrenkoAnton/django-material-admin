081501944a07cbf59ee0c72b7d4d190a7f8a5e91
Anton <it2015maistrenko@gmail.com>
2019-07-15 12:53:29 +0300

    Bump to v1.1.20

--- package/setup.py
+++ package/setup.py
@@ -10,7 +10,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name="django-material-admin",
-    version="1.1.19",
+    version="1.1.20",
     license='MIT License',
     packages=find_packages(),
     author="Anton Maistrenko",
