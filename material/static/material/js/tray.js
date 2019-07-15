a79ffea038d85d8aada45a6cdcfe84edee1356ad
Anton <it2015maistrenko@gmail.com>
2019-07-15 13:53:39 +0300

    Side Nav in Tray (#32)
    * Collapse Side Nav to Tray

---
+++ material/static/material/js/tray.js
@@ -0,0 +1,47 @@
+(function ($) {
+
+    var navBarIcon = $('.tray-nav-bar.material-icons');
+    var navBarCookie = 'tray-nav-bar';
+
+    function createCookie(name, value, days) {
+        var expires;
+
+        if (days) {
+            var date = new Date();
+            date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+            expires = "; expires=" + date.toGMTString();
+        } else {
+            expires = "";
+        }
+        document.cookie = encodeURIComponent(name) + "=" + encodeURIComponent(value) + expires + "; path=/";
+    }
+
+    function readCookie(name) {
+        var nameEQ = encodeURIComponent(name) + "=";
+        var ca = document.cookie.split(';');
+        for (var i = 0; i < ca.length; i++) {
+            var c = ca[i];
+            while (c.charAt(0) === ' ')
+                c = c.substring(1, c.length);
+            if (c.indexOf(nameEQ) === 0)
+                return decodeURIComponent(c.substring(nameEQ.length, c.length));
+        }
+        return null;
+    }
+
+    function eraseCookie(name) {
+        createCookie(name, "", -1);
+    }
+
+    $('.nav-bar.minimize').on('click', function () {
+        createCookie(navBarCookie, true, 30);
+        location.reload();
+    });
+
+    navBarIcon.on('click', function () {
+        eraseCookie(navBarCookie);
+        location.reload();
+    });
+
+
+})(jQuery);
--- material/templatetags/material.py
+++ material/templatetags/material.py
@@ -25,3 +25,10 @@ def template_exists(value):
         return True
     except template.TemplateDoesNotExist:
         return False
+
+
+@register.simple_tag(takes_context=True)
+def cookie(context, cookie_name):
+    request = context['request']
+    result = request.COOKIES.get(cookie_name)
+    return result == 'true'
