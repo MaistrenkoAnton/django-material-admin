53d45eab0700050f14f9f9a440154c3465219fdc
Anton <it2015maistrenko@gmail.com>
2019-07-18 09:55:52 +0300

    Check if not request in context to fetch cookies.

--- material/templatetags/material.py
+++ material/templatetags/material.py
@@ -31,6 +31,8 @@ def template_exists(value):
 
 @register.simple_tag(takes_context=True)
 def cookie(context, cookie_name):
+    if 'request' not in context:
+        return False
     request = context['request']
     result = request.COOKIES.get(cookie_name)
     return result == 'true'
