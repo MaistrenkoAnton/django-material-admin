25b48d44f4230251362ff7121cbd0535b99c1fa9
Anton <it2015maistrenko@gmail.com>
2019-11-26 14:59:13 +0200

    Bump to v1.5.3

--- package/setup.py
+++ package/setup.py
@@ -10,7 +10,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name="django-material-admin",
-    version="1.5.2",
+    version="1.5.3",
     license='MIT License',
     packages=find_packages(),
     author="Anton Maistrenko",
