333409520eb71e28f4418a99d72378c407f3cd90
Anton <it2015maistrenko@gmail.com>
2019-12-27 20:34:13 +0200

    Fix overflow for choice fields in inline forms.

--- app/demo/documents/models.py
+++ app/demo/documents/models.py
@@ -23,6 +23,24 @@ class Link(models.Model):
     url = models.URLField(_('Url'))
     document = models.ForeignKey('documents.Document', verbose_name=_('Document'), null=True, on_delete=models.CASCADE)
     is_approved = models.BooleanField(_('Approved'), default=False)
+    type = models.CharField(
+        _('Type'),
+        choices=(
+            ('choice1', 'Choice1'),
+            ('choice2', 'Choice2'),
+            ('choice3', 'Choice3'),
+            ('choice4', 'Choice4'),
+            ('choice5', 'Choice5'),
+            ('choice6', 'Choice6'),
+            ('choice7', 'Choice7'),
+            ('choice8', 'Choice8'),
+            ('choice9', 'Choice9'),
+            ('choice10', 'Choice10'),
+            ('choice11', 'Choice11'),
+        ),
+        null=True,
+        max_length=64
+    )
 
     class Meta:
         verbose_name = _('Link')
--- app/demo/relations/admin.py
+++ app/demo/relations/admin.py
@@ -3,8 +3,8 @@ from django.contrib.admin import ModelAdmin, register
 from demo.relations.models import Relation
 
 
-# @register(Relation)
-# class RelationAdmin(ModelAdmin):
-#     list_display = ('id', 'name')
-#     icon_name = 'layers'
-#     autocomplete_fields = ('documents', 'user')
+@register(Relation)
+class RelationAdmin(ModelAdmin):
+    list_display = ('id', 'name')
+    icon_name = 'layers'
+    autocomplete_fields = ('documents', 'user')
