62ed77cd83a3df7574bdc09ca96c0626c9c1a99a
Anton <it2015maistrenko@gmail.com>
2020-01-16 10:53:34 +0200

    Fix style for help text. Bump to v1.6.31

--- package/setup.py
+++ package/setup.py
@@ -10,7 +10,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name="django-material-admin",
-    version="1.6.30",
+    version="1.6.31",
     license='MIT License',
     packages=find_packages(),
     author="Anton Maistrenko",
