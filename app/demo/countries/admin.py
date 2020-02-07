d50def1713507b21b1a1e394dc3422df6d4d8602
Anton <it2015maistrenko@gmail.com>
2020-02-07 17:08:49 +0200

    Load jquery ui via https. Refactor.

--- app/demo/countries/admin.py
+++ app/demo/countries/admin.py
@@ -23,9 +23,9 @@ class CountryAdmin(TranslationAdmin):
 
     class Media:
         js = (
-            'http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js',
-            'http://ajax.googleapis.com/ajax/libs/jqueryui/1.10.2/jquery-ui.min.js',
-            'modeltranslation/js/tabbed_translation_fields1.js',
+            'https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js',
+            'https://ajax.googleapis.com/ajax/libs/jqueryui/1.10.2/jquery-ui.min.js',
+            'modeltranslation/js/tabbed_translation_fields.js',
         )
         css = {
             'screen': ('modeltranslation/css/tabbed_translation_fields.css',),
