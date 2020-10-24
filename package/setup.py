5cca0e141d4be6b55d683847a6be871ec861f06f
amaistrenko <amaistrenko@s-pro.io>
2020-10-24 11:07:24 +0300

    Bump to 1.7.9

--- package/setup.py
+++ package/setup.py
@@ -10,7 +10,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name="django-material-admin",
-    version="1.7.8",
+    version="1.7.9",
     license='MIT License',
     packages=find_packages(),
     author="Anton Maistrenko",
