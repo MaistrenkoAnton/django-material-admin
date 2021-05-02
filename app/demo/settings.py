cf54893c462414f0cb5e8e61f0a74381c1b1060e
Anton <it2015maistrenko@gmail.com>
2021-05-02 11:49:58 +0300

    test: fix env vars for pg

--- app/demo/settings.py
+++ app/demo/settings.py
@@ -117,11 +117,11 @@ WSGI_APPLICATION = 'demo.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'HOST': os.environ.get('DB_HOST'),
-        'NAME': os.environ.get('DB_NAME'),
-        'USER': os.environ.get('DB_USER'),
-        'PASSWORD': os.environ.get('DB_PASS'),
-        'PORT': '5432',
+        'HOST': os.environ.get('POSTGRES_HOST'),
+        'NAME': os.environ.get('POSTGRES_DB'),
+        'USER': os.environ.get('POSTGRES_USER'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+        'PORT': os.environ.get('POSTGRES_PORT'),
     }
 }
 
--- docker/start.sh
+++ docker/start.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+then while ! (timeout 3 bash -c "</dev/tcp/${POSTGRES_HOST}/${POSTGRES_PORT}") &> /dev/null;
+do
+  echo waiting for PostgreSQL to start...;
+  sleep 3;
+done;
+
 ./manage.py migrate  --no-input --traceback
 ./manage.py collectstatic --no-input --traceback
 ./manage.py runserver 0.0.0.0:8000
